dbedf2479240fee59db14f244e58c77a33757c78
Md. Rasheduzzaman <44283694+Rashed112@users.noreply.github.com>
2022-09-20 11:30:01 +0600

    solution with hints
    greedy, sortings

---
+++ Codeforces/1733A-Consecutive Sum.cpp
@@ -0,0 +1,30 @@
+/*
+Hint: we don't have to care about the number after k index, so we have to store the max values in  i%k indexes
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+int main()
+{
+    ll t, n, k;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>k;
+        vector< int > v(n);
+        int x;
+        for(int i=0;i<n;i++){
+            cin>>x;
+            v[i%k]= max(v[i%k], x);
+        }
+        ll sum=0;
+        for(int i=0;i<k;i++){
+            sum+=v[i];
+        }
+        cout<<sum<<endl;
+    }
+}
