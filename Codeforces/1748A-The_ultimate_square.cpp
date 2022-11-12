645a05745690da1a6a117b8b6a378e65b9310d20
Md. Rasheduzzaman <44283694+Rashed112@users.noreply.github.com>
2022-11-12 22:43:02 +0600

    Create 1748A-The_ultimate_square.cpp

---
+++ Codeforces/1748A-The_ultimate_square.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ll t, n, res;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        res = ceil((n*1.0)/2);
+        cout<<res<<endl;
+    }
+}
