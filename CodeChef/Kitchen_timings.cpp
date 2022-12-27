671d7cc617bb010ef2bdc1f126db8ace0be0d7cf
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2022-12-28 00:34:19 +0600

    Create Kitchen_timings.cpp

---
+++ CodeChef/Kitchen_timings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t, x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    cout<<y-x<<endl;
+	    
+	}
+	return 0;
+}
