3e23ffd171b5f03ce614919500460c91ac620f71
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-04-26 23:35:12 +0600

    Create 1822C_bun_lover.cpp

---
+++ Codeforces/1822C_bun_lover.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    int q;
+    cin>>q;
+    while(q--){
+        ll n;
+        cin>>n;
+        ll res;
+        res=n*(n+2)+2;
+        cout<<res<<endl;
+        
+    }
+}
