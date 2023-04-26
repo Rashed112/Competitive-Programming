1d30f0ad7aa3d94043f913ff868f8d0f78486529
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-04-26 23:29:09 +0600

    Create existence.cpp

---
+++ CodeChef/existence.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    ll x,y;
+	    cin>>x>>y;
+	    ll X=x*x;
+        ll Y=(2*y);
+        //cout<<X<<" hi "<<Y<<endl;
+	    
+	    if(X==Y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	    
+	}
+	return 0;
+}
