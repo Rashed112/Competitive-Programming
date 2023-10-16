492527a8852b400219710c296683f1ad84dfe301
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-10-16 12:45:05 +0600

    predicative_bs

---
+++ SPOJ/AGGRCOW.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin()) //returns pos equal or greater than of x
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 200100;
+const int N  = 1e5+10;
+
+int n, cows;
+int positions[N];
+
+bool canPlaceCows(int minDist){
+    int lastPos = -1;
+    int cows_ct = cows;
+    for(int i=0;i<n;i++){
+        if(positions[i]-lastPos>=minDist || lastPos==-1){
+            cows_ct--;
+            lastPos = positions[i];
+        }
+        if(cows_ct==0){
+            break;
+        }
+    }
+    return cows_ct==0;
+}
+
+int main() {
+    _ios;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        cin>>n>>cows;
+        for(int i=0;i<n;i++){
+            cin>>positions[i];
+        }
+        sort(positions, positions+n);
+        //TTTTTFFFFFFF
+        int lo = 0, hi = 1e9, mid;
+        while(hi-lo>1){
+            mid = lo+(hi-lo)/2;
+            if(canPlaceCows(mid)){
+                lo = mid;
+            }
+            else{
+                hi = mid-1;
+            }
+        }
+        if(canPlaceCows(hi)){
+            cout<<hi<<endl;
+        }
+        else{
+            cout<<lo<<endl;
+        }
+    }
+}
