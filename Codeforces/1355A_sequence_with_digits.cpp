ef4e792fc16a4bd690f605e581af5acfc84a5a26
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-12-05 13:03:11 +0600

    Create 1355A_sequence_with_digits.cpp

---
+++ Codeforces/1355A_sequence_with_digits.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+#define fill(a) memset(a, 0, sizeof(a))
+#define all(v) (v).begin(), (v).end()
+#define sp(k) cout << setprecision(k) << fixed;
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define irep(i, b, a) for (int i = (b); i >= (a); --i)
+#define minv(v) *min_element(v.begin(), v.end())
+#define maxv(v) *max_element(v.begin(), v.end())
+#define each(it, s) for (auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)), (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define getpos(v, x) (lower_bound(v.begin(), v.end(), x) - v.begin()) // returns pos equal or greater than of x
+#define _ios                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define nl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 200100;
+const int N = 10000007;
+
+void solve()
+{
+    ll n, currN, nextN;
+    ll k;
+    cin >> n >> k;
+    currN = n;
+    if(k==1){
+        cout<<n<<endl;
+        return;
+    }
+    for (ll i = 2; i <= k; i++)
+    {
+        ll minN = mod, maxN = 0, temp = currN;
+        while(temp>0)
+        {
+
+            minN = min(minN, temp%10);
+            maxN = max(maxN, temp%10);
+            temp/=10;
+        }
+        nextN = currN + minN * maxN;
+        if (minN == 0)
+        {
+            cout << nextN << endl;
+            return;
+        }
+        currN = nextN;
+    }
+    cout << nextN << endl;
+}
+
+int main()
+{
+    _ios;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
