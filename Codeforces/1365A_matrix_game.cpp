e78fa208aff1056ed05fd8f2e2aa15db1fe574b9
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2023-12-30 15:19:33 +0600

    Create 1365A_matrix_game.cpp

---
+++ Codeforces/1365A_matrix_game.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+#define fill(a) memset(a, 0, sizeof(a))
+#define all(v) (v).begin(), (v).end()
+#define sp(k) cout << setprecision(k) << fixed;
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define irep(i, b, a) for (int i = (b); i >= (a); --i)
+#define minv(v) *min_element(v.begin(), v.end())
+#define maxv(v) *max_element(v.begin(), v.end())
+#define each(it, s) for (auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)), (v).erase(unique((v).begin(), (v).end()), (v).end())
+#define getpos(v, x) (lower_bound(v.begin(), v.end(), x) - v.begin()) // returns pos equal or greater than of x
+#define _ios                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define nl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 200100;
+const int N = 10000007;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> v(n);
+    vector<int> row(n, 0), col(m, 0);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int x;
+            cin >> x;
+            v[i].push_back(x);
+            row[i] += x;
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            col[i] += v[j][i];
+        }
+    }
+    int r=0, c=0;
+    for(int i=0;i<n;i++){
+        if(row[i]==0){
+            r++;
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        if (col[i] == 0)
+        {
+            c++;
+        }
+    }
+    int turn = min(r, c);
+    if(turn%2){
+        cout<<"Ashish"<<endl;
+    }
+    else{
+        cout<<"Vivek"<<endl;
+    }
+}
+
+int main()
+{
+    _ios;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
