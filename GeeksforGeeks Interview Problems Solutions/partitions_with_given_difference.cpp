e7c29519d6581d4eb32e3313ae63f5883ebcaec1
Md. Rasheduzzaman <rashedzaman319@gmail.com>
2024-05-29 15:57:23 +0600

    dp

---
+++ GeeksforGeeks Interview Problems Solutions/partitions_with_given_difference.cpp
@@ -0,0 +1,27 @@
+int countPartitions(int n, int d, vector<int>& arr) {
+  int sum = accumulate(arr.begin(), arr.end(), 0);
+  if((sum+d)%2 != 0){
+      return 0;
+  }
+  int target = (d+sum)/2;
+  int mod = 1e9+7;
+  vector<vector<int>>dp(n+1, vector<int>(target+1,0));
+  
+  for(int i=0;i<=n;i++){
+      for(int j=0;j<=target; j++){
+          if(i==0 && j==0){
+              dp[i][j]=1;
+          }
+          else if(i==0){
+              dp[i][j]=0;
+          }
+          else if(arr[i-1]<=j){
+              dp[i][j] = (dp[i-1][j-arr[i-1]] + dp[i-1][j]) % mod;
+          }
+          else {
+              dp[i][j] = dp[i-1][j] % mod;
+          }
+      }
+  }
+  return dp[n][target];
+}
